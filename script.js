3aa4a887e36dcba55fe66d96dc32c7997bf7b43b
Donovan <triops124@gmail.com>
2020-07-01 19:12:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 const queryString = window.location.search;
 var checksubmit = (urlParams.has('submit'));
-if (checksubmit == True) {
+if (checksubmit) {
 $(".formy").hide();
 document.getElementById('formy').style.display = 'none';
 var media1 = urlParams.get('media1');
