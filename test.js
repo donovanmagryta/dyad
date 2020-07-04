2075c25d2685cd8018adc590287e4d74afe6635c
Donovan <triops124@gmail.com>
2020-07-04 13:50:44 -0400

    Update test.js

--- test.js
+++ test.js
@@ -6,12 +6,12 @@ alert(checksubmit);
 $(document).ready(function(){
 $(".formy").hide();
 $(".medy").show();
- }
+ });
 }
 else {
  alert("checksubmit not in url query");
  $(document).ready(function(){
  $(".formy").show();
  $(".medy").hide();
-  }
+  });
 }
