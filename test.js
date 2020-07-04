b08d60ba5c9d9f20ecee931974cdcd595371e8f6
Donovan <triops124@gmail.com>
2020-07-04 13:30:27 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,25 +3,29 @@ var checksubmit = arr[1];
 if (checksubmit) {
 alert("checksubmit sent");
 alert(checksubmit);
- var x = document.getElementById("formy");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+ //var x = document.getElementById("formy");
+ // if (x.style.display === "none") {
+   // x.style.display = "block";
+ // } else {
+    //x.style.display = "none";
+ // }
 //document.getElementById('formy').style.display = 'none';
-//$(".formy").hide();
-//$(".medy").show();
+ $(document).ready(function(){
+$(".formy").hide();
+$(".medy").show();
 }
+                   }
 else {
 alert("checksubmit not in url query");
- var y = document.getElementById("medy");
-  if (y.style.display === "none") {
-    y.style.display = "block";
-  } else {
-    y.style.display = "none";
+ //var y = document.getElementById("medy");
+  //if (y.style.display === "none") {
+    ///y.style.display = "block";
+ // } else {
+   //// y.style.display = "none";
   }
 //document.getElementById('medy').style.display = 'none';
-//$(".formy").show();
-//$(".medy").hide();
+$(document).ready(function(){
+$(".formy").show();
+$(".medy").hide();
+}
 }
