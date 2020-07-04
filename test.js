3356218e75eabc590deb05948a239843b53febbe
Donovan <triops124@gmail.com>
2020-07-04 13:09:15 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,11 @@ var checksubmit = arr[1];
 if (checksubmit) {
 alert("checksubmit sent");
 alert(checksubmit);
+$(".formy").hide();
+$(".medy").show();
 }
 else {
 alert("checksubmit not in url query");
+$(".formy").show();
+$(".medy").hide();
 }
