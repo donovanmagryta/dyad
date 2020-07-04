2568b7c7fcbfcae1af8439fd53b3b58e88f04aba
Donovan <triops124@gmail.com>
2020-07-04 17:36:52 -0400

    Update test.js

--- test.js
+++ test.js
@@ -47,7 +47,7 @@ alert("media2:");
 alert(media2);
 document.getElementById('media1').src = media1;
 document.getElementById('media2').src = media2;
-document.getElementById("buttonlink").value=buttonlink;
+document.getElementById("buttonlink").innerHTML=buttonlink;
 document.getElementById("buttontitle").value=buttontitle;
 }
 //}
