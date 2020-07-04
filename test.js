6d124268aec69198b2c03796bf12854ebc823ec4
Donovan <triops124@gmail.com>
2020-07-04 13:42:49 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,28 +3,15 @@ var checksubmit = arr[1];
 if (checksubmit) {
 alert("checksubmit sent");
 alert(checksubmit);
- //var x = document.getElementById("formy");
- // if (x.style.display === "none") {
-   // x.style.display = "block";
- // } else {
-    //x.style.display = "none";
- // }
-//document.getElementById('formy').style.display = 'none';
- $(document).ready(function(){
+$(document).ready(function(){
 $(".formy").hide();
 $(".medy").show();
+ }
 }
-                   }
 else {
-alert("checksubmit not in url query");
- //var y = document.getElementById("medy");
-  //if (y.style.display === "none") {
-    ///y.style.display = "block";
- // } else {
-   //// y.style.display = "none";
-//document.getElementById('medy').style.display = 'none';
-$(document).ready(function(){
-$(".formy").show();
-$(".medy").hide();
-}
+ alert("checksubmit not in url query");
+ $(document).ready(function(){
+ $(".formy").show();
+ $(".medy").hide();
+  }
 }
