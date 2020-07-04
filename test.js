a4721d61e1eabb2e8cef35b245d62a7d7c3cf105
Donovan <triops124@gmail.com>
2020-07-04 14:32:07 -0400

    Update test.js

--- test.js
+++ test.js
@@ -2,15 +2,15 @@ var arr = document.URL.match(/submit=([0-9]+)/);
 //var checksubmit = arr[1];
 //if (checksubmit) {
 if (z) {
-//alert("checksubmit sent");
-//alert(checksubmit);
+alert("checksubmit sent");
+alert(checksubmit);
 $(document).ready(function(){
 $(".formy").hide();
 $(".medy").show();
  });
 }
 else {
- //alert("checksubmit not in url query");
+ alert("checksubmit not in url query");
  $(document).ready(function(){
  $(".formy").show();
  $(".medy").hide();
