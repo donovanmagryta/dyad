a4fa17d9bb24eb6c7bbf4fa3059c747e840e1813
Donovan <triops124@gmail.com>
2020-07-04 14:59:11 -0400

    Update test.js

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ jQuery(".media").show();
  });
 var media = document.URL.match(/media=([0-9]+)/);
 var media = media[1];
-var mediae= decodeURIComponent(media.replace(/\+/g, '%20') );
+var media = decodeURIComponent(media.replace(/\+/g, '%20') );
 document.getElementById('media').src = media;
 }
 else {
