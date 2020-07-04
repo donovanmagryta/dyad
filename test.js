dc49ae118881aa3f272099bd87f9219c15abea52
Donovan <triops124@gmail.com>
2020-07-04 15:14:44 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,14 +3,16 @@ if (arr) {
 var checksubmit = arr[1];
 if (checksubmit) {
 alert("checksubmit sent");
-alert(checksubmit);
+// alert(checksubmit);
 jQuery(document).ready(function(){
 jQuery(".form").hide();
 jQuery(".media").show();
  });
 var media = document.URL.match(/media=([0-9]+)/);
 var media = media[1];
+alert(media);
 var media = decodeURIComponent(media.replace(/\+/g, '%20') );
+alert(media);
 document.getElementById('media').src = media;
 }
 }
