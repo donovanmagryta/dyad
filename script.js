2035c6b4d10012468d92f5ef4fe145a1fdd63659
Donovan <triops124@gmail.com>
2020-07-12 16:51:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function getQueryVariable(variable) {
+var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
+var media1 = getQueryVariable("media1");
+var media2 = getQueryVariable("media2");
+var media1type = getQueryVariable("media1type");
+var media2type = getQueryVariable("media12type");
+var buttontitle = getQueryVariable("buttontitle");
+var buttonlink = getQueryVariable("buttonlink");
+var checksubmit = getQueryVariable("submit");
+if (checksubmit) {
+jQuery(document).ready(function(){
+jQuery(".formy").hide();
+ jQuery(".medy").show();
+ });
+var media1 = decodeURIComponent(media1.replace(/\+/g, '%20') );
+var media2 = decodeURIComponent(media2.replace(/\+/g, '%20') );
+var buttonlink = decodeURIComponent(buttonlink.replace(/\+/g, '%20') );
+var buttontitle = decodeURIComponent(buttontitle.replace(/\+/g, '%20') );
+document.getElementById('media1').src = media1;
+document.getElementById('media2').src = media2;
+var d1 = document.getElementById('button1');
+d1.insertAdjacentHTML('afterend', '<a href="'+ buttonlink +'"><button style="font-size:50px;background-color:red;margin: 4px 2px;padding: 5px 5px;color:white;border:none;border-radius:10px;text-decoration:none;" >' + buttontitle + ' </button></a>');
+}
+else {
+ jQuery(document).ready(function(){
+ jQuery(".formy").show();
+ jQuery(".medy").hide();
+  });
+}
