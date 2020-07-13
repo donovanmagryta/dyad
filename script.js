ddfbfbc89e6e71789efe50d80d95b9cbb774ec90
Donovan <triops124@gmail.com>
2020-07-12 20:20:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -15,19 +15,21 @@ var buttontitle = getQueryVariable("buttontitle");
 var buttonlink = getQueryVariable("buttonlink");
 var checksubmit = getQueryVariable("submit");
 if (checksubmit) {
+var media1 = decodeURIComponent(media1.replace(/\+/g, '%20') );
+var media2 = decodeURIComponent(media2.replace(/\+/g, '%20') );
+var buttonlink = decodeURIComponent(buttonlink.replace(/\+/g, '%20') );
+var buttontitle = decodeURIComponent(buttontitle.replace(/\+/g, '%20') );
+ if (media1type == "video") {
 jQuery(document).ready(function(){
 jQuery(".formy").hide();
  jQuery(".medy").show();
  jQuery(".yt").hide();
  });
-var media1 = decodeURIComponent(media1.replace(/\+/g, '%20') );
-var media2 = decodeURIComponent(media2.replace(/\+/g, '%20') );
-var buttonlink = decodeURIComponent(buttonlink.replace(/\+/g, '%20') );
-var buttontitle = decodeURIComponent(buttontitle.replace(/\+/g, '%20') );
 document.getElementById('media1').src = media1;
 document.getElementById('media2').src = media2;
 var d1 = document.getElementById('button1');
 d1.insertAdjacentHTML('afterend', '<a href="'+ buttonlink +'"><button style="font-size:50px;background-color:red;margin: 4px 2px;padding: 5px 5px;color:white;border:none;border-radius:10px;text-decoration:none;" >' + buttontitle + ' </button></a>');
+ }
 if (media1type == "youtube") {
 jQuery(document).ready(function(){
 jQuery(".formy").hide();
@@ -36,9 +38,8 @@ jQuery(".formy").hide();
  });
 let re = /^(https?:\/\/)?((www\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.*(v\/|v=|vi=|vi\/|e\/|embed\/|user\/.*\/u\/\d+\/)|youtu\.be\/)([_0-9a-z-]+)/i; // added 7-12-2020;
 let idone = media1.match(re)[7]; // added 7-12-2020
-//alert(id);
 var media1 = 'https://youtube.com/embed/' + idone + '?autoplay=1';
-//alert(media1);
+alert(media1);
 document.getElementById('yt1').src = media1;
 let idtwo = media2.match(re)[7]; // added 7-12-2020
 //alert(id);
