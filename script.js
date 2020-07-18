f876549faf4e2c327ff9ba030115154c139a1b42
Donovan <triops124@gmail.com>
2020-07-18 08:32:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ var d2 = document.getElementById('button2');
 d2.insertAdjacentHTML('afterend', '<a href="'+ buttonlink +'"><button style="background-color:#DA4336;margin: 4px 2px;padding: 5px 5px;color:white;border:none;border-radius:10px;text-decoration:none;" >' + buttontitle + ' </button></a>');
  }
 else if (media1type == "youtube") {
-//alert("case 2 youtube");
+alert("case 2 youtube");
 jQuery(document).ready(function(){
 jQuery(".formy").hide();
  jQuery(".medy").hide();
@@ -43,14 +43,73 @@ let re = /^(https?:\/\/)?((www\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.
 let idone = media1.match(re)[7]; // added 7-12-2020
 var media1 = 'https://youtube.com/embed/' + idone + '?autoplay=1';
 //alert(media1);
-document.getElementById('yt1').src = media1;
+//document.getElementById('yt1').src = media1;
 let idtwo = media2.match(re)[7]; // added 7-12-2020
 //alert(id);
 var media2 = 'https://youtube.com/embed/' + idtwo + '?autoplay=1';
 //alert(media2);
-document.getElementById('yt2').src = media2;
+//document.getElementById('yt2').src = media2;
 var d1 = document.getElementById('button1');
 d1.insertAdjacentHTML('afterend', '<a href="'+ buttonlink +'"><button style="background-color:#DA4336;margin: 4px 2px;padding: 5px 5px;color:white;border:none;border-radius:10px;text-decoration:none;" >' + buttontitle + ' </button></a>');
+// added 7-18-2020
+ // Load the IFrame Player API code asynchronously.
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  const video_container = document.getElementById('video_container');
+  const videos = [media1, media2];
+
+
+  const players = [];
+       
+       
+   function mute(){ //need to modify for either/or not both videos
+    players.forEach(function(p){
+      p.playVideo();
+    })
+  }
+  
+  function playAll(){
+    players.forEach(function(p){
+      p.playVideo();
+    })
+  }
+
+  function pauseAll(){
+    players.forEach(function(p){
+      p.stopVideo();
+    })
+  }
+
+  function state_changed(e) {
+    console.log('state changed', e)
+    // PLAYING
+    if(e.data === 1) {
+      playAll();
+    } 
+    // PAUSED
+    if(e.data === 2){
+      pauseAll();
+    }
+  }
+
+
+  function onYouTubePlayerAPIReady() {
+    videos.forEach(function(v){
+      const el = document.createElement('div');
+      video_container.append(el);
+      const player = new YT.Player(el, {
+        videoId: v,
+        events: {
+          onStateChange: state_changed,
+        },
+      });
+      players.push(player);
+    });
+// added 7-18-2020      
+       
 }
 
 }
