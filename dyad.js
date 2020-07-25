901cfcf9ab3b4fcad635d14240274c7a5ec92635
Donovan <triops124@gmail.com>
2020-07-24 20:51:58 -0400

    Update dyad.js

--- dyad.js
+++ dyad.js
@@ -169,11 +169,11 @@ function onPlayerReady(event) {
   event.target.playVideo();
   var title1 =  player1.getVideoData().title;
   var title2 =  player2.getVideoData().title;
-alert(title1);
-alert(title2);
-//var title = title1 + "  X  " title2;
-//alert(title);
-//document.querySelector('meta[property="og:title"]').setAttribute("content", title);
+//alert(title1);
+//alert(title2);
+var title = title1 + "  X  " + title2;
+alert(title);
+document.querySelector('meta[property="og:title"]').setAttribute("content", title);
 }
 }       
 }
