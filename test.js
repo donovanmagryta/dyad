cf41819e99adba85a58e29b10492e233da8f0022
Donovan <triops124@gmail.com>
2020-07-25 20:22:44 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,35 +1,18 @@
+jQuery(document).ready(function(){
+var lank = window.location.href;
+//var lank = "https://google.com";
+//alert(lank);
+var geturl = "https://api.urlday.com/short?url=" + lank;
+//alert(geturl);
+ jQuery(".buttn").click(function(){
+    jQuery.get(geturl, function(data, status){
 
-function shortEn() {
-//var currentURL = "https://google.com";
-var currentURL = window.location.href;
-  alert(currentURL);
- jQuery(document).ready(function(){
-
-  // jQuery methods go here...
-
-jQuery.get("https://api.urlday.com/short?url="+currentURL+"", function(data, status){
-                                      alert("Data: " + data);
-var obj = JSON.parse(data);
-var paste=(obj.result);
-//alert(JSON.stringify(data));
-alert(JSON.stringify(paste));
-                              });
-                            
-});                            
-
-//}
-  /* Get the text field */
-  //var copyText = //document.getElementById("myInput");
-
-  /* Select the text field */
-  //copyText.select();
- // copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-
-  /* Copy the text inside the text field */
-  //currentURL.execCommand("copy");  
-
-  /* Alert the copied text 
-  alert("Copied the link for this dyad: " + data);
-  */
-}
-
+      var hi = JSON.stringify(data.result);
+      str = hi.slice(1, -1);
+      alert(str);
+     $(".dummy").html(str);
+    
+  });
+  });
+  
+});
