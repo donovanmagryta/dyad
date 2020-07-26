4d26db680e29005fd31e30e2f12a63df4bc672c4
Donovan <triops124@gmail.com>
2020-07-25 23:11:52 -0400

    Update dyad.js

--- dyad.js
+++ dyad.js
@@ -6,7 +6,7 @@ var query = window.location.search.substring(1);
                if(pair[0] == variable){return pair[1];}
        }
        return(false);
-}
+};
 
 function copyToClip(str) {
   function listener(e) {
@@ -18,8 +18,8 @@ function copyToClip(str) {
   document.execCommand("copy");
   document.removeEventListener("copy", listener);
   alert("Link to this duo copied to clipboard");
-}
-
+};
+/*
 function sharLink() {
 //var lank = window.location.href;
  var lank = "https://dyad.link/index.html?media1="+media1+"%26media2="+media2+"%26buttonlink="+buttonlink+"%26buttontitle="+buttontitle+"%26media1type="+media1type+"%26media2type="+media1type+"+"%26submit=submit";
@@ -32,6 +32,7 @@ alert(geturl);
       jQuery("#den").html(str);
     });
 }
+*/
 
 
 var media1 = getQueryVariable("media1");
