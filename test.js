2ed4bd62b23ec0799ad11f0425868ba387c6a005
Donovan <triops124@gmail.com>
2020-07-26 15:09:29 -0400

    Update test.js

--- test.js
+++ test.js
@@ -37,7 +37,7 @@ var checksubmit = getQueryVariable("submit");
 var tester = getQueryVariable("tester");
 	var windy = "?media1="+media1+"%26media2="+media2+"%26buttonlink="+buttonlink+"%26buttontitle="+buttontitle+"%26media1type="+media1type+"%26media2type="+media1type+"%26submit=submit";
     /// First, let's compress it.
-    my_lzma.compress(windy, compression_mode, function on_compress_complete(result) {
+    /*my_lzma.compress(windy, compression_mode, function on_compress_complete(result) {
         //alert("Compressed querystring: " + result);
 	    var clunky = "https://dyad.link/#" + result; //untested
 	    alert("compressed:" + clunky);
@@ -50,6 +50,21 @@ var tester = getQueryVariable("tester");
         /// Compressing progress code goes here.
         document.title = "Compressing: " + (percent * 100) + "%";
     });
+    */
+	
+	my_lzma.compress(windy, compression_mode, function on_compress_complete(result) {
+        alert("Compressed: " + result);
+        var clunky = "https://dyad.link/#" + result; //untested
+	 alert("compressed:" + clunky);
+        //  window.location = clunky; //untested
+        
+        }, function on_decompress_progress_update(percent) {
+            
+        });
+    }, function on_compress_progress_update(percent) {
+        
+    });
+
 }
 
 
