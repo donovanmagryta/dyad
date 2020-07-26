a75a6e5baeab0aac53f08a361237249ea8c5eb4b
Donovan <triops124@gmail.com>
2020-07-26 13:55:53 -0400

    Update dyad.js

--- dyad.js
+++ dyad.js
@@ -67,9 +67,9 @@ alert("url querystring:" + windy);
 function comPressy() {
     /// First, let's compress it.
     my_lzma.compress(windy, compression_mode, function on_compress_complete(result) {
-        alert("Compressed querystring: " + result);
-	    var clunky = "https://dyad.link#" + result; //untested
-	    alert("shortened url:" + clunky);
+        //alert("Compressed querystring: " + result);
+	    var clunky = "https://dyad.link/#" + result; //untested
+	    alert(clunky);
           window.location = clunky; //untested
 	   var typ = window.location.hash.substr(1); //untested
         /// Now, let's try to decompress it to make sure it works both ways.
