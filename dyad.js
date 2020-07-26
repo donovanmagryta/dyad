25de6a5422b20a0e25171d96ae2d1345ef675ae4
Donovan <triops124@gmail.com>
2020-07-26 12:05:41 -0400

    Update dyad.js

--- dyad.js
+++ dyad.js
@@ -31,21 +31,27 @@ function sharLink() {
 //var lank = window.location.href;
        //var lank = "https://google.com";
        // url encoded ampersands works but %20 space fails.
-var lank = "https://dyad.link/index.html?media1="+media1+"%26media2="+media2+"%26buttonlink="+buttonlink+"%26buttontitle="+buttontitle+"%26media1type="+media1type+"%26media2type="+media1type+"%26submit=submit";
-//alert(lank);
+var lank = "?media1="+media1+"%26media2="+media2+"%26buttonlink="+buttonlink+"%26buttontitle="+buttontitle+"%26media1type="+media1type+"%26media2type="+media1type+"%26submit=submit";
+var blenk = lank.compress();
+       var blienk = "https://dyad.link#" + blenk;
+       window.location = blienk;
+       
+       //alert(lank);
        //try without encoding ampersands
       // var lank = "https://dyad.link/index.html?media1="+media1+"&media2="+media2+"&buttonlink="+buttonlink+"&buttontitle="+buttontitle+"&media1type="+media1type+"&media2type="+media1type+"&submit=submit";
 //alert(lank);
-var geturl = "https://api.urlday.com/short?url=" + lank;
+//var geturl = "https://api.urlday.com/short?url=" + lank;
 //alert(geturl);
-    jQuery.get(geturl, function(data, status){
-    var hi = JSON.stringify(data.result);
-      str = hi.slice(1, -1);
+   // jQuery.get(geturl, function(data, status){
+    //var hi = JSON.stringify(data.result);
+     // str = hi.slice(1, -1);
       jQuery("#den").html(str);
-    });
+  //  });
 }
-
-
+var typ = window.location.hash.substr(1);
+var clink = typ.uncompress();
+var clunky = "https://dyad.link?" + clink;
+window.location = clunky;
 
 var media1 = getQueryVariable("media1");
 var media2 = getQueryVariable("media2");
@@ -248,3 +254,95 @@ document.querySelectorAll("video").forEach(vid => vid.pause());
 document.getElementsByTagName("video")[0].removeAttribute("controls");
 document.getElementsByTagName("video")[1].removeAttribute("controls");
 }
+
+
+
+String.prototype.compress = function (asArray) {
+	"use strict";
+	// Build the dictionary.
+	asArray = (asArray === true);
+	var i,
+		dictionary = {},
+		uncompressed = this,
+		c,
+		wc,
+		w = "",
+		result = [],
+		ASCII = '',
+		dictSize = 256;
+	for (i = 0; i < 256; i += 1) {
+		dictionary[String.fromCharCode(i)] = i;
+	}
+
+	for (i = 0; i < uncompressed.length; i += 1) {
+		c = uncompressed.charAt(i);
+		wc = w + c;
+		//Do not use dictionary[wc] because javascript arrays
+		//will return values for array['pop'], array['push'] etc
+	   // if (dictionary[wc]) {
+		if (dictionary.hasOwnProperty(wc)) {
+			w = wc;
+		} else {
+			result.push(dictionary[w]);
+			ASCII += String.fromCharCode(dictionary[w]);
+			// Add wc to the dictionary.
+			dictionary[wc] = dictSize++;
+			w = String(c);
+		}
+	}
+
+	// Output the code for w.
+	if (w !== "") {
+		result.push(dictionary[w]);
+		ASCII += String.fromCharCode(dictionary[w]);
+	}
+	return asArray ? result : ASCII;
+};
+
+String.prototype.decompress = function () {
+	"use strict";
+	// Build the dictionary.
+	var i, tmp = [],
+		dictionary = [],
+		compressed = this,
+		w,
+		result,
+		k,
+		entry = "",
+		dictSize = 256;
+	for (i = 0; i < 256; i += 1) {
+		dictionary[i] = String.fromCharCode(i);
+	}
+
+	if(compressed && typeof compressed === 'string') {
+		// convert string into Array.
+		for(i = 0; i < compressed.length; i += 1) {
+			tmp.push(compressed[i].charCodeAt(0));
+		}
+		compressed = tmp;
+		tmp = null;
+	}
+
+	w = String.fromCharCode(compressed[0]);
+	result = w;
+	for (i = 1; i < compressed.length; i += 1) {
+		k = compressed[i];
+		if (dictionary[k]) {
+			entry = dictionary[k];
+		} else {
+			if (k === dictSize) {
+				entry = w + w.charAt(0);
+			} else {
+				return null;
+			}
+		}
+
+		result += entry;
+
+		// Add w+entry[0] to the dictionary.
+		dictionary[dictSize++] = w + entry.charAt(0);
+
+		w = entry;
+	}
+	return result;
+};
