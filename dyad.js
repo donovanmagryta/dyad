78887935d9d4eb1a9871c791561c16285f326692
Donovan <triops124@gmail.com>
2020-07-29 10:50:24 -0400

    Update dyad.js

--- dyad.js
+++ dyad.js
@@ -44,8 +44,7 @@ var buttontitle = decodeURIComponent(buttontitle.replace(/\+/g, '%20') );
   //var lanke = encodeURIComponent(lonky);
   var lanky = btoa(lonky);
   var newlank = "https://dyad.link/?duo=" + lanky;
-       alert(newlank);
-//var newlank = encodeURIComponent(newlank);
+      // alert(newlank);
 window.location = newlank;
 }
 
@@ -109,27 +108,17 @@ jQuery(".formy").hide();
  function metAhh() { 
   var title1 =  player1.getVideoData().title;
   var title2 =  player2.getVideoData().title;
-//alert(title1);
-//alert(title2);
 var title = title1 + "  X  " + title2;
 alert(title);
-document.querySelector('meta[property="og:title"]').setAttribute("content", title);
-        document.querySelector('meta[property="og:description"]').setAttribute("description", " + title +" );
+//document.querySelector('meta[property="og:title"]').setAttribute("content", title);
+//document.querySelector('meta[property="og:description"]').setAttribute("description", title);
 //jQuery('meta[property="og:title"]').replaceWith('<meta property="og:title" content=title>');
 //jQuery('meta[property="og:description"]').replaceWith('<meta property="og:description" content=" + title + ">');
 }
 
 let re = /^(https?:\/\/)?((www\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.*(v\/|v=|vi=|vi\/|e\/|embed\/|user\/.*\/u\/\d+\/)|youtu\.be\/)([_0-9a-z-]+)/i; // added 7-12-2020;
 let idone = media1.match(re)[7]; // added 7-12-2020
-//alert(idone);
-//var media1 = 'https://youtube.com/embed/' + idone + '?autoplay=1';
-//alert(media1);
-//document.getElementById('yt1').src = media1;
 let idtwo = media2.match(re)[7]; // added 7-12-2020
-//alert(idtwo);
-//var media2 = 'https://youtube.com/embed/' + idtwo + '?autoplay=1';
-//alert(media2);
-//document.getElementById('yt2').src = media2;
 var d1 = document.getElementById('button1');
 d1.insertAdjacentHTML('afterend', '<a href="'+ buttonlink +'"><button style="background-color:#DA4336;margin: 4px 2px;padding: 5px 5px;color:white;border:none;border-radius:10px;text-decoration:none;" >' + buttontitle + ' </button></a>');
 // added 7-18-2020
@@ -165,10 +154,7 @@ function onYouTubeIframeAPIReady() {
     }
   });
 
-  // After loading the iframe, set the "playVideo" onclick event on "playButton" anchor element.
-  ///document.getElementById('playButton').onclick = function() {
-   // player1.playVideo();
- // };
+  
 
 }
 
@@ -186,26 +172,13 @@ function onPlayerStateChange(event) {
     document.getElementById('player1').onmouseover = function() {
      player1.unMute();
       player2.mute();
-    //};
+    
   }
   
   document.getElementById('player2').onmouseover = function() {
       player2.unMute();
       player1.mute();
     };
-    
-    function swapSound() {
-  if (player1.isMuted()) {
-    alert('player1 not muted');
-  player2.unMute();
-  }
-  else if (player2.isMuted()) {
-  alert('player 2 not muted');
-  player1.unMute();
-  }
-}
-    
-    
   }
 
   // If the video is PAUSED, set the onclick element for pause the video.
