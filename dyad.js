061fd2a4d5b036216a1bf20dd6e7efffe3e1a0a8
Donovan <triops124@gmail.com>
2020-07-28 22:09:41 -0400

    Update dyad.js

--- dyad.js
+++ dyad.js
@@ -115,8 +115,8 @@ var title = title1 + "  X  " + title2;
 alert(title);
 document.querySelector('meta[property="og:title"]').setAttribute("content", title);
         document.querySelector('meta[property="og:description"]').setAttribute("description", " + title +" );
-jQuery('meta[property="og:title"]').replaceWith('<meta property="og:title" content=title>');
-jQuery('meta[property="og:description"]').replaceWith('<meta property="og:description" content=" + title + ">');
+//jQuery('meta[property="og:title"]').replaceWith('<meta property="og:title" content=title>');
+//jQuery('meta[property="og:description"]').replaceWith('<meta property="og:description" content=" + title + ">');
 }
 
 let re = /^(https?:\/\/)?((www\.)?(youtube(-nocookie)?|youtube.googleapis)\.com.*(v\/|v=|vi=|vi\/|e\/|embed\/|user\/.*\/u\/\d+\/)|youtu\.be\/)([_0-9a-z-]+)/i; // added 7-12-2020;
