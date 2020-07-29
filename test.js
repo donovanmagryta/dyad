a8a7b9f35b2f10a0716ff2ce6420e3c6425fafe3
Donovan <triops124@gmail.com>
2020-07-29 11:17:37 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,25 +1,2 @@
 /* FOR TESTING NEW FEATURES */
 
-// parse values in linear order from comma separated url fragment.
-// ie; location = "https://example.com/#video,https:/example.com/vid.mp4,subscribe/";
-     var query =  window.location.hash.substr(1);
-      var vars = query.split("~");
-      var media = vars[0];
-      var source = vars[1];
-      var title = vars[2];
-
-
-//Get URL query parameter values.
-//ie: location = "https://example.com?media=video&source=https:\/\/example.com\/vid.mp4\/title=funny+video/";
-function getQueryVariable(variable) {
-var query = window.location.search.substring(1);
-       var vars = query.split("&");
-       for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] == variable){return pair[1];}
-       }
-       return(false);
-}
-var video = getQueryVariable("media");
-var videosource = getQueryVariable("source");
-var title = getQueryVariable("title");
