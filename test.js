8254c122771b297839b38f467c7c65efa3001733
Donovan <triops124@gmail.com>
2020-07-29 12:05:00 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
 /* FOR TESTING NEW FEATURES */
-
+var title = "test-title";
+var desc = "test-description";
+document.querySelector('meta[property="og:title"]').setAttribute("content", title);
+document.querySelector('meta[property="og:description"]').setAttribute("description", desc);
+//jQuery('meta[property="og:title"]').replaceWith('<meta property="og:title" content=title>');
+//jQuery('meta[property="og:description"]').replaceWith('<meta property="og:description" content=" + title + ">');
